d735bb8f02f58a9868c72a9409628e152f1989c3
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-06-03 12:15:01 +0200

    added informations about the lack of toppings

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const input = document.querySelector(".search");
 const listItems = document.querySelectorAll(".list-item");
+const info = document.querySelector(".info");
 
 const searchEngine = (e) => {
 	const text = e.target.value.toLowerCase();
 
-	listItems.forEach(listItem => {
+	listItems.forEach((listItem) => {
 		if (listItem.textContent.indexOf(text) !== -1) {
 			listItem.style.display = "block";
 		} else {
 			listItem.style.display = "none";
 		}
 	});
+
+	const areAllHidden = Array.from(listItems).every(
+		(listItem) => listItem.style.display === "none"
+	);
+
+	if (areAllHidden) {
+		info.style.display = "block";
+	} else {
+		info.style.display = "none";
+	}
 };
 
 input.addEventListener("keyup", searchEngine);
